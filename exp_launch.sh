5c3b52a676f44d50f183d9b0d6ab70ab25b00aa5
devinaa <devinaa.k19@gmail.com>
2025-05-22 10:59:18 +0800

    changed file path

--- exp_launch.sh
+++ exp_launch.sh
@@ -2,7 +2,7 @@
 
 xterm -iconic -hold -e "python postercombiner.py" &
 sleep 3; xterm -iconic -hold -e "python gui.py" &
-sleep 1; xterm -iconic -hold -e "python combined_joystick.py" &
+sleep 1; xterm -iconic -hold -e "python joystick/combined_joystick.py" &
 #sleep 1; xterm -iconic -hold -e "python modified_combined_joystick.py" &
-sleep 1; xterm -iconic -hold -e "python sonar_test/sonar_get_min_dist.py"&
+#sleep 1; xterm -iconic -hold -e "python sonar_test/sonar_get_min_dist.py"&
 sleep 1; xterm -iconic -hold -e "./tcp-server 8080 8081"
