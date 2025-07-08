903ccd91a27fbe393a04c951a54a46545a7e30ea
Ribena-dev <devinaa.k19@gmail.com>
2025-07-08 14:34:23 +0700

    kernel interfaces is detached but usb is still busy

--- reward_pyusb.py
+++ reward_pyusb.py
@@ -6,31 +6,29 @@ import usb.core
 # for device in devices:
 # 	print ("vendor:",device.idVendor,"Product:", device.idProduct)
 
-dev = usb.core.find(idVendor=0x04D8,idProduct=0x00DD)
-if dev:
+device = usb.core.find(idVendor=0x04D8,idProduct=0x00DD)
+if device:
 	print("found MCP2221")
 else:
 	print("no MCP2221 found")
-
-try:
-        dev.set_configuration()  # Try without detaching first
-        print("Device available, no driver detachment needed")
-except usb.core.USBError as e:
-        if "busy" in str(e).lower():
-            print("Device busy, will detach driver...")
-            # Only detach if really necessary
-            if dev.is_kernel_driver_active(0):
-                dev.detach_kernel_driver(0)
-            dev.set_configuration()
+print("checking if kernel tracking")
+if device.is_kernel_driver_active(1) is True:
+	device.detach_kernel_driver(0)
+	device.detach_kernel_driver(1)
+	print("kernel driver detached")
+elif device.is_kernel_driver_active(0) is False:
+	print("no kernel attached")
+	
+device.set_configuration()
 def reward(pin_number):
-	dev.set_configuration()
+	device.set_configuration()
 	cmd = [0x50]+[0x00] *63
 	pin_offset = 7 + (pin_number * 4)
 	cmd[pin_offset] = 0x01      # Set as output
 	cmd[pin_offset + 1] = 0x01  # Set value high
 	
 	# Send command to device
-	dev.write(0x01, cmd)
+	device.write(0x3, cmd)
 
 	return 
 
